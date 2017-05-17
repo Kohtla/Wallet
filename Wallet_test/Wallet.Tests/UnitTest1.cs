f6ee1cda93db7ce33791cd01216c6886f38ffaa2
DESKTOP-SF1NBDO\krenog <krenog@gmail.com>
2017-05-17 21:24:58 +0400

    First

--- Wallet_test/Wallet.Tests/UnitTest1.cs
+++ Wallet_test/Wallet.Tests/UnitTest1.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Wallet.Tests
+using Wallet_test;
+namespace Wallet_Tests
 {
     [TestClass]
     public class UnitTest1
@@ -9,6 +10,39 @@ namespace Wallet.Tests
         [TestMethod]
         public void TestMethod1()
         {
+            Wallet kek=new Wallet();
+            kek.addMoney("RUB", 500);
+            Assert.AreEqual(kek.getMoney("RUB"),500);
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Wallet kek = new Wallet();
+            kek.addMoney("RUB", 500);
+            kek.addMoney("RUB", 1000);
+            Assert.AreEqual(kek.getMoney("RUB"), 1500);
+
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Wallet kek = new Wallet();
+            kek.addMoney("RUB", 500);
+            kek.addMoney("EUR", 1000);
+            Assert.AreEqual(kek.getCount(), 2);
+
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Wallet kek = new Wallet();
+            kek.addMoney("RUB", 500);
+            kek.addMoney("EUR", 1000);
+            Assert.AreEqual(kek.toString2(),"500 RUB,1000 EUR");
+
         }
     }
 }
+
--- Wallet_test/Wallet_test/Wallet.cs
+++ Wallet_test/Wallet_test/Wallet.cs
@@ -1,12 +1,78 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Wallet_test
 {
-    class Wallet
+   public class Wallet
     {
+        Hashtable wall = new Hashtable();
+        public Wallet(){
+            
+            }
+   
+        public void addMoney(string n, int amount)
+        {
+            if (wall[n] == null)
+            {
+                wall[n] = amount;
+
+            }
+            else {
+                wall[n] = (int)wall[n]+ amount;
+            }
+
+        }
+        int tryremov(string n, int amount) {
+            if ((int)wall[n] < amount)
+            {
+                throw new System.OverflowException();
+
+            }
+            else
+            {
+                
+                return (int)wall[n] - amount;
+            }
+            }
+        public void removeMoney(string n, int amount)
+        {
+            try
+            {
+                wall[n] = tryremov(n, amount);
+                if ((int)wall[n] == 0) wall.Remove(n);
+            }
+            catch(OverflowException e) {
+                    
+            }
+
+        }
+        public int getMoney(string n) {
+            if (wall[n] == null)
+            {
+                return (int)wall[n];
+            }
+            else return 0;
+        }
+        public int getCount() {
+            string str = "";
+            ICollection leys = wall.Keys;
+            return wall.Count;
+
+        }
+        public string toString2()
+        {
+            string str = "";
+            ICollection leys = wall.Keys;
+            foreach (string s in leys) {
+                str = str +  wall[s].ToString() + " " + s + ",";
+            }
+           str=str.Substring(0, str.Length- 1);
+            return str;
+        }
     }
 }
