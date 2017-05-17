93582309cd61d5bd7f3a58f559307953c7b25079
DESKTOP-SF1NBDO\krenog <krenog@gmail.com>
2017-05-17 22:18:28 +0400

    2ецщ

--- Wallet_test/Wallet.Tests/UnitTest1.cs
+++ Wallet_test/Wallet.Tests/UnitTest1.cs
@@ -10,7 +10,8 @@ namespace Wallet_Tests
         [TestMethod]
         public void TestMethod1()
         {
-            Wallet kek=new Wallet();
+            Bank b = new Bank();
+            Wallet kek=new Wallet(b);
             kek.addMoney("RUB", 500);
             Assert.AreEqual(kek.getMoney("RUB"),500);
 
@@ -18,7 +19,8 @@ namespace Wallet_Tests
         [TestMethod]
         public void TestMethod2()
         {
-            Wallet kek = new Wallet();
+            Bank b = new Bank();
+            Wallet kek = new Wallet(b);
             kek.addMoney("RUB", 500);
             kek.addMoney("RUB", 1000);
             Assert.AreEqual(kek.getMoney("RUB"), 1500);
@@ -28,7 +30,8 @@ namespace Wallet_Tests
         [TestMethod]
         public void TestMethod3()
         {
-            Wallet kek = new Wallet();
+            Bank b = new Bank();
+            Wallet kek = new Wallet(b);
             kek.addMoney("RUB", 500);
             kek.addMoney("EUR", 1000);
             Assert.AreEqual(kek.getCount(), 2);
@@ -37,10 +40,23 @@ namespace Wallet_Tests
         [TestMethod]
         public void TestMethod4()
         {
-            Wallet kek = new Wallet();
+            Bank b = new Bank();
+            Wallet kek = new Wallet(b);
             kek.addMoney("RUB", 500);
             kek.addMoney("EUR", 1000);
             Assert.AreEqual(kek.toString2(),"500 RUB,1000 EUR");
+
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Bank b = new Bank();
+            //Wallet kek = new Wallet(b);
+            //kek.addMoney("USD", 100);
+            //kek.addMoney("EUR", 100);
+            //int total = kek.getTotalMoney("RUB");
+           int total= b.convert(100, "USD", "RUB");
+            Assert.AreEqual(total, 6000);
 
         }
     }
--- Wallet_test/Wallet_test/Wallet.cs
+++ Wallet_test/Wallet_test/Wallet.cs
@@ -11,15 +11,16 @@ namespace Wallet_test
    public class Wallet
     {
         Hashtable wall = new Hashtable();
-        public Wallet(){
-            
+        Bank bank;
+        public Wallet(Bank n1){
+            bank = n1;
             }
    
         public void addMoney(string n, int amount)
         {
             if (wall[n] == null)
             {
-                wall[n] = amount;
+                wall.Add(n, amount);
 
             }
             else {
@@ -73,6 +74,51 @@ namespace Wallet_test
             }
            str=str.Substring(0, str.Length- 1);
             return str;
+        }
+        public int getTotalMoney(string a1)
+        {
+            int total=0;
+            ICollection leys = wall.Keys;
+            foreach(string s in leys)
+            {
+                if (s == a1)
+                {
+                    total = total + (int)wall[s];
+                }
+                else {
+                    total = total + bank.convert((int)wall[s], s, a1);
+                }
+
+            }
+            return total;
+
+        }
+    }
+
+
+    public class Bank {
+        Hashtable curse = new Hashtable();
+        public Bank() {
+            //RUB, USD, GBP
+            curse.Add("RUBUSD", 20);
+            curse.Add("RUBGPB", 16);
+            curse.Add("USDRUB",60000);
+            curse.Add("USDGPB", 850);
+            curse.Add("GPBRUB", 70000);
+            curse.Add("GPBUSD", 1200);
+
+        }
+
+        public int convert(int amount, string n1, string n2) {
+            Random a = new Random();
+            //  int rnd = a.Next(100, 120);
+            string str = n1 + n2;
+            int l1 = (int)curse[str];
+            //int rnd = 100;
+            //double m1 = (double)curse[n1 + n2];
+            int kek = l1 * amount;
+            return kek/1000;
+
         }
     }
 }
